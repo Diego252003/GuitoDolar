aea11be66e6bf7805dee7e82c135c4338b75b863
Diego252003 <diegoagd2003@gmail.com>
2025-02-08 23:11:50 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html", // Or your main HTML file
+      "./src/**/*.{js,ts,jsx,tsx}", // Path to your component files (adjust as needed)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
